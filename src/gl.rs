4ecf86462945a1409896646b71f6e91a12926443
Roi Martin <jroi.martin@gmail.com>
2024-12-06 22:58:00 +0100

    gl: implement functions required for OpenGL's hello triangle

--- src/gl.rs
+++ src/gl.rs
@@ -2,7 +2,7 @@
 
 use std::{
     ffi::{c_void, CStr},
-    ptr,
+    mem, ptr,
     sync::Mutex,
 };
 
@@ -27,6 +27,7 @@ mod ffi {
     pub type GLclampf = c_float;
     pub type GLdouble = c_double;
     pub type GLclampd = c_double;
+    pub type GLsizeiptr = usize;
 
     macro_rules! glfn {
         ($name:ident, $once:ident, $ret:ty $(, $pname:ident: $ptype:ty)*) => {
@@ -42,11 +43,30 @@ mod ffi {
         }
     }
 
+    glfn![glAttachShader, GL_ATTACH_SHADER, (), program: GLuint, shader: GLuint];
+    glfn![glBindBuffer, GL_BIND_BUFFER, (), target: GLenum, buffer: GLuint];
+    glfn![glBindVertexArray, GL_BIND_VERTEX_ARRAY, (), array: GLuint];
+    glfn![glBufferData, GL_BUFFER_DATA, (), target: GLenum, size: GLsizeiptr, data: *const c_void, usage: GLenum];
     glfn![glClear, GL_CLEAR, (), mask: GLbitfield];
     glfn![glClearColor, GL_CLEAR_COLOR, (), red: GLclampf, green: GLclampf, blue: GLclampf, alpha: GLclampf];
+    glfn![glCompileShader, GL_COMPILE_SHADER, (), shader: GLuint];
+    glfn![glCreateProgram, GL_CREATE_PROGRAM, GLuint];
+    glfn![glCreateShader, GL_CREATE_SHADER, GLuint, shaderType: GLenum];
     glfn![glDebugMessageCallback, GL_DEBUG_MESSAGE_CALLBACK, (), callback: *const c_void, userParam: *const c_void];
+    glfn![glDeleteBuffers, GL_DELETE_BUFFERS, (), n: GLsizei, buffers: *mut GLuint];
+    glfn![glDeleteProgram, GL_DELETE_PROGRAM, (), program: GLuint];
+    glfn![glDeleteShader, GL_DELETE_SHADER, (), shader: GLuint];
+    glfn![glDeleteVertexArrays, GL_DELETE_VERTEX_ARRAYS, (), n: GLsizei, arrays: *mut GLuint];
+    glfn![glDrawArrays, GL_DRAW_ARRAYS, (), mode: GLenum, first: GLint, count: GLsizei];
     glfn![glEnable, GL_ENABLE, (), cap: GLenum];
+    glfn![glEnableVertexAttribArray, GL_ENABLE_VERTEX_ATTRIB_ARRAY, (), index: GLuint];
+    glfn![glGenBuffers, GL_GEN_BUFFERS, (), n: GLsizei, buffers: *mut GLuint];
+    glfn![glGenVertexArrays, GL_GEN_VERTEX_ARRAYS, (), n: GLsizei, arrays: *mut GLuint];
     glfn![glGetError, GL_GET_ERROR, GLenum];
+    glfn![glLinkProgram, GL_LINK_PROGRAM, (), program: GLuint];
+    glfn![glShaderSource, GL_SHADER_SOURCE, (), shader: GLuint, count: GLsizei, string: *const *const GLchar, length: *const GLint];
+    glfn![glUseProgram, GL_USE_PROGRAM, (), program: GLuint];
+    glfn![glVertexAttribPointer, GL_VERTEX_ATTRIB_POINTER, (), index: GLuint, size: GLint, typ: GLenum, normalized: GLboolean, stride: GLsizei, pointer: *const c_void];
     glfn![glViewport, GL_VIEWPORT, (), x: GLint, y: GLint, width: GLsizei, height: GLsizei];
 }
 
@@ -56,6 +76,53 @@ pub const COLOR_BUFFER_BIT: u32 = 0x00004000;
 /// If enabled, debug messages are produced by a debug context.
 pub const DEBUG_OUTPUT: u32 = 0x92e0;
 
+/// Vertex attributes.
+pub const ARRAY_BUFFER: u32 = 0x8892;
+
+/// The data store contents are modified by the application, and used
+/// as the source for GL drawing and image specification commands. The
+/// data store contents will be modified once and used many times.
+pub const STATIC_DRAW: u32 = 0x88E4;
+
+/// Fragment shader type.
+pub const FRAGMENT_SHADER: u32 = 0x8B30;
+
+/// Vertext shader type.
+pub const VERTEX_SHADER: u32 = 0x8B31;
+
+/// Float data type.
+pub const FLOAT: u32 = 0x1406;
+
+/// Triangles primitive.
+pub const TRIANGLES: u32 = 0x0004;
+
+/// Attaches a shader object to a program object.
+pub fn attach_shader(program: u32, shader: u32) {
+    unsafe { ffi::glAttachShader(program, shader) }
+}
+
+/// Binds a named buffer object.
+pub fn bind_buffer(target: u32, buffer: u32) {
+    unsafe { ffi::glBindBuffer(target, buffer) }
+}
+
+/// Binds a vertex array object.
+pub fn bind_vertex_array(array: u32) {
+    unsafe { ffi::glBindVertexArray(array) }
+}
+
+/// Creates and initializes a buffer object's data store.
+pub fn buffer_data<T>(target: u32, data: &[T], usage: u32) {
+    unsafe {
+        ffi::glBufferData(
+            target,
+            mem::size_of_val(data),
+            data.as_ptr() as *const c_void,
+            usage,
+        )
+    }
+}
+
 /// Clears buffers to preset values.
 pub fn clear(mask: u32) {
     unsafe { ffi::glClear(mask) }
@@ -64,6 +131,21 @@ pub fn clear(mask: u32) {
 /// Specifies clear values for the color buffers.
 pub fn clear_color(red: f32, green: f32, blue: f32, alpha: f32) {
     unsafe { ffi::glClearColor(red, green, blue, alpha) }
+}
+
+/// Compiles a shader object.
+pub fn compile_shader(shader: u32) {
+    unsafe { ffi::glCompileShader(shader) }
+}
+
+/// Creates a program object.
+pub fn create_program() -> u32 {
+    unsafe { ffi::glCreateProgram() }
+}
+
+/// Creates a shader object.
+pub fn create_shader(typ: u32) -> u32 {
+    unsafe { ffi::glCreateShader(typ) }
 }
 
 type FnError = fn(source: u32, typ: u32, id: u32, severity: u32, length: i32, message: &str);
@@ -95,14 +177,101 @@ pub fn debug_message_callback(callback: FnError) {
     unsafe { ffi::glDebugMessageCallback(error_callback as *const c_void, ptr::null()) }
 }
 
+/// Deletes named buffer objects.
+pub fn delete_buffers(buffers: Vec<u32>) {
+    unsafe { ffi::glDeleteBuffers(buffers.len() as i32, buffers.as_ptr() as *mut u32) }
+}
+
+/// Deletes a program object.
+pub fn delete_program(program: u32) {
+    unsafe { ffi::glDeleteProgram(program) }
+}
+
+/// Deletes a shader object.
+pub fn delete_shader(shader: u32) {
+    unsafe { ffi::glDeleteShader(shader) }
+}
+
+/// Deletes vertex array objects.
+pub fn delete_vertex_arrays(arrays: Vec<u32>) {
+    unsafe { ffi::glDeleteVertexArrays(arrays.len() as i32, arrays.as_ptr() as *mut u32) }
+}
+
+/// Renders primitives from array data.
+pub fn draw_arrays(mode: u32, first: i32, count: i32) {
+    unsafe { ffi::glDrawArrays(mode, first, count) }
+}
+
 /// Enables server-side GL capabilities.
 pub fn enable(cap: u32) {
     unsafe { ffi::glEnable(cap) }
 }
 
+/// Enables a generic vertex attribute array.
+pub fn enable_vertex_attrib_array(index: u32) {
+    unsafe { ffi::glEnableVertexAttribArray(index) }
+}
+
+/// Generates buffer object names.
+pub fn gen_buffers(n: usize) -> Vec<u32> {
+    let buffers = vec![0u32; n];
+    unsafe { ffi::glGenBuffers(n as i32, buffers.as_ptr() as *mut ffi::GLuint) };
+    buffers
+}
+
+/// Generates vertex array object names.
+pub fn gen_vertex_arrays(n: usize) -> Vec<u32> {
+    let arrays = vec![0u32; n];
+    unsafe { ffi::glGenVertexArrays(n as i32, arrays.as_ptr() as *mut ffi::GLuint) };
+    arrays
+}
+
 /// Returns the value of the error flag.
 pub fn get_error() -> u32 {
     unsafe { ffi::glGetError() }
+}
+
+/// Links a program object.
+pub fn link_program(program: u32) {
+    unsafe { ffi::glLinkProgram(program) }
+}
+
+/// Replaces the source code in a shader object.
+pub fn shader_source(shader: u32, sources: &[&str]) {
+    let count = sources.len();
+    let strings: Vec<*const ffi::GLchar> = sources
+        .iter()
+        .map(|s| s.as_ptr() as *const ffi::GLchar)
+        .collect();
+    let lengths: Vec<i32> = sources.iter().map(|s| s.len() as i32).collect();
+    unsafe { ffi::glShaderSource(shader, count as i32, strings.as_ptr(), lengths.as_ptr()) }
+}
+
+/// Installs a program object as part of current rendering state.
+pub fn use_program(program: u32) {
+    unsafe { ffi::glUseProgram(program) }
+}
+
+/// Defines an array of generic vertex attribute data.
+pub fn vertex_attrib_pointer(
+    index: u32,
+    size: usize,
+    typ: u32,
+    normalized: bool,
+    stride: usize,
+    pointer: usize,
+) {
+    let normalized = if normalized { 1 } else { 0 };
+    unsafe {
+        ffi::glVertexAttribPointer(
+            index,
+            size as i32,
+            typ,
+            normalized,
+            stride as i32,
+            pointer as *const c_void,
+        )
+    }
 }
 
 /// Sets the viewport.
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
 //! Hitchcock is a libre demo creation tool.
 
+use std::mem;
+
 use hitchcock::{gl, glfw};
 
 const INITIAL_WIDTH: i32 = 800;
 const INITIAL_HEIGHT: i32 = 600;
+
+const VERTICES: [f32; 9] = [-0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0];
+
+const VERTEX_SHADER_SOURCE: &str = r#"
+    #version 330 core
+    layout (location = 0) in vec3 aPos;
+
+    void main()
+    {
+        gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    }
+"#;
+
+const FRAGMENT_SHADER_SOURCE: &str = r#"
+    #version 330 core
+    out vec4 FragColor;
+
+    void main()
+    {
+        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    }
+"#;
 
 fn main() {
     glfw::init().unwrap();
@@ -16,21 +40,55 @@ fn main() {
 
     let window =
         glfw::create_window(INITIAL_WIDTH, INITIAL_HEIGHT, "Hitchcock", None, None).unwrap();
-
     glfw::make_context_current(window);
+    glfw::set_framebuffer_size_callback(window, Some(glfw_framebuffer_size_callback));
 
     gl::enable(gl::DEBUG_OUTPUT);
     gl::debug_message_callback(gl_error_callback);
 
-    glfw::set_framebuffer_size_callback(window, Some(glfw_framebuffer_size_callback));
+    let vertex_shader = gl::create_shader(gl::VERTEX_SHADER);
+    gl::shader_source(vertex_shader, &[VERTEX_SHADER_SOURCE]);
+    gl::compile_shader(vertex_shader);
+
+    let fragment_shader = gl::create_shader(gl::FRAGMENT_SHADER);
+    gl::shader_source(fragment_shader, &[FRAGMENT_SHADER_SOURCE]);
+    gl::compile_shader(fragment_shader);
+
+    let shader_program = gl::create_program();
+    gl::attach_shader(shader_program, vertex_shader);
+    gl::attach_shader(shader_program, fragment_shader);
+    gl::link_program(shader_program);
+    gl::delete_shader(vertex_shader);
+    gl::delete_shader(fragment_shader);
+
+    let vaos = gl::gen_vertex_arrays(1);
+    gl::bind_vertex_array(vaos[0]);
+
+    let vbos = gl::gen_buffers(1);
+    gl::bind_buffer(gl::ARRAY_BUFFER, vbos[0]);
+    gl::buffer_data(gl::ARRAY_BUFFER, &VERTICES, gl::STATIC_DRAW);
+
+    gl::vertex_attrib_pointer(0, 3, gl::FLOAT, false, 3 * mem::size_of::<f32>(), 0);
+    gl::enable_vertex_attrib_array(0);
+
+    gl::bind_buffer(gl::ARRAY_BUFFER, 0);
+    gl::bind_vertex_array(0);
 
     while !glfw::window_should_close(window) {
-        gl::clear_color(1.0, 0.0, 0.0, 1.0);
+        gl::clear_color(0.2, 0.3, 0.3, 1.0);
         gl::clear(gl::COLOR_BUFFER_BIT);
+
+        gl::use_program(shader_program);
+        gl::bind_vertex_array(vaos[0]);
+        gl::draw_arrays(gl::TRIANGLES, 0, 3);
 
         glfw::swap_buffers(window);
         glfw::poll_events();
     }
+
+    gl::delete_vertex_arrays(vaos);
+    gl::delete_buffers(vbos);
+    gl::delete_program(shader_program);
 
     glfw::terminate()
 }
@@ -40,7 +98,7 @@ fn glfw_error_callback(error_code: glfw::ErrorCode, description: &str) {
 }
 
 fn glfw_framebuffer_size_callback(_window: glfw::Window, width: i32, height: i32) {
-    eprintln!("resize window: size={}x{}", width, height);
+    gl::viewport(0, 0, width, height);
 }
 
 fn gl_error_callback(source: u32, typ: u32, id: u32, severity: u32, length: i32, message: &str) {
