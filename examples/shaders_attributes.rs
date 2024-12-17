05af770495c785228b5144a92cf0626e05fd1554
Roi Martin <jroi.martin@gmail.com>
2024-12-17 17:55:23 +0100

    examples/shaders_attributes: add "Shaders" lesson part 2 of LearnOpenGL

--- examples/shaders_attributes.rs
+++ examples/shaders_attributes.rs
@@ -1,7 +1,7 @@
-//! [Hello Triangle] lesson of LearnOpenGL. This example introduces
-//! element buffer objects.
+//! [Shaders] lesson of LearnOpenGL. This example introduces
+//! additional vertex attributes.
 //!
-//! [Hello Triangle]: https://learnopengl.com/Getting-started/Hello-Triangle
+//! [Shaders]: https://learnopengl.com/Getting-started/Shaders
 
 use std::{mem, process};
 
@@ -10,35 +10,35 @@ use hitchcock::{gl, glfw, Result};
 const INITIAL_WIDTH: i32 = 800;
 const INITIAL_HEIGHT: i32 = 600;
 
-const VERTICES: [f32; 12] = [
-    0.5, 0.5, 0.0, // top right
-    0.5, -0.5, 0.0, // bottom right
-    -0.5, -0.5, 0.0, // bottom left
-    -0.5, 0.5, 0.0, // top left
-];
-
-const INDICES: [u32; 6] = [
-    0, 1, 3, // right triangle
-    1, 2, 3, // left triangle
+const VERTICES: [f32; 18] = [
+    -0.5, -0.5, 0.0, 1.0, 0.0, 0.0, // Bottom Left, Red.
+    0.5, -0.5, 0.0, 0.0, 1.0, 0.0, // Bottom Right, Green.
+    0.0, 0.5, 0.0, 0.0, 0.0, 1.0, // Top, Blue.
 ];
 
 const VERTEX_SHADER_SOURCE: &str = r#"
     #version 330 core
     layout (location = 0) in vec3 aPos;
+    layout (location = 1) in vec3 aColor;
+
+    out vec3 fColor;
 
     void main()
     {
-        gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+        gl_Position = vec4(aPos, 1.0);
+        fColor = aColor;
     }
     "#;
 
 const FRAGMENT_SHADER_SOURCE: &str = r#"
     #version 330 core
+    in vec3 fColor;
+
     out vec4 FragColor;
 
     void main()
     {
-        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+        FragColor = vec4(fColor, 1.0);
     }
     "#;
 
@@ -61,7 +61,7 @@ fn example() -> Result<()> {
     let window = glfw::create_window(
         INITIAL_WIDTH,
         INITIAL_HEIGHT,
-        "LearnOpenGL: Hello Triangle with Element Buffer Objects",
+        "LearnOpenGL: Shaders",
         None,
         None,
     )?;
@@ -88,15 +88,21 @@ fn example() -> Result<()> {
 
     let vaos = gl::gen_vertex_arrays(1);
     let vbos = gl::gen_buffers(1);
-    let ebos = gl::gen_buffers(1);
 
     gl::bind_vertex_array(vaos[0]);
     gl::bind_buffer(gl::ARRAY_BUFFER, vbos[0]);
     gl::buffer_data(gl::ARRAY_BUFFER, &VERTICES, gl::STATIC_DRAW);
-    gl::bind_buffer(gl::ELEMENT_ARRAY_BUFFER, ebos[0]);
-    gl::buffer_data(gl::ELEMENT_ARRAY_BUFFER, &INDICES, gl::STATIC_DRAW);
-    gl::vertex_attrib_pointer(0, 3, gl::FLOAT, false, 3 * mem::size_of::<f32>(), 0);
+    gl::vertex_attrib_pointer(0, 3, gl::FLOAT, false, 6 * mem::size_of::<f32>(), 0);
     gl::enable_vertex_attrib_array(0);
+    gl::vertex_attrib_pointer(
+        1,
+        3,
+        gl::FLOAT,
+        false,
+        6 * mem::size_of::<f32>(),
+        3 * mem::size_of::<f32>(),
+    );
+    gl::enable_vertex_attrib_array(1);
     gl::bind_buffer(gl::ARRAY_BUFFER, gl::Buffer::zero());
     gl::bind_vertex_array(gl::VertexArray::zero());
 
@@ -108,14 +114,13 @@ fn example() -> Result<()> {
 
         gl::use_program(shader_program);
         gl::bind_vertex_array(vaos[0]);
-        gl::draw_elements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, 0);
+        gl::draw_arrays(gl::TRIANGLES, 0, 3);
 
         glfw::swap_buffers(window);
     }
 
     gl::delete_vertex_arrays(&vaos);
     gl::delete_buffers(&vbos);
-    gl::delete_buffers(&ebos);
     gl::delete_program(shader_program);
 
     glfw::terminate();
